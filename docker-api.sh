9cbc28a5ad8a3ec5f340e3d56dba708a616373f1
Cola <pezihan@qq.com>
2026-01-07 23:27:00 +0800

    up

--- docker-api.sh
+++ docker-api.sh
@@ -46,7 +46,7 @@ docker rmi ${image_tag}:update
 echo "=== 启动新容器 ==="
 docker run --restart=always \
  --name ${image_tag} \
- -v /home/ubuntu/tmp:/tmp \
+ -v /home/ubuntu/files:/files \
  -p 80:5001 \
  -d ${image_tag}:latest
 
